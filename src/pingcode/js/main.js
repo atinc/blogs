0105a0ef8dce8f85ea1c8c542dd2070ea1bd1e9d
尹晓斌 <yxb941006@163.com>
2020-11-16 14:55:06 +0800

    feat: #LDP-188 博客文章分享到微信群时，支持LOGO展示 (#11)
    * feat: #LDP-188 博客文章分享到微信群时，支持LOGO展示
    
    * feat: add https

--- src/pingcode/js/main.js
+++ src/pingcode/js/main.js
@@ -1,3 +1,7 @@
+const jquery = require('jquery');
+const BLOG_SHARE_APP_ID = 'wxedc101aec3d70448';
+const HOST = location.hostname === 'localhost' ? 'http://pingcode.live' : 'https://pingcode.com';
+
 function headerMenu() {
   const doms = {
     headerMenu: document.querySelector(".header-menu"),
@@ -51,8 +55,59 @@ function checkWxProgram() {
   }
 }
 
+function getShareConfig() {
+  jquery.ajax({
+    method: "GET",
+    url: HOST + '/api/siren/wechat/signature',
+    data: { url: encodeURIComponent(location.href) }
+  })
+    .then(data => {
+      console.log('response data:>> ', data);
+      console.log('wx.config :>> ', wx.config);
+      const { nonceStr, signature, timestamp } = data.data.value;
+      wx.config({
+        debug: false,
+        appId: BLOG_SHARE_APP_ID,
+        timestamp,
+        nonceStr,
+        signature,
+        jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData'],
+      });
+    })
+    .done(function (msg) {
+      console.log('msg :>> ', msg);
+      const title = document.querySelector('meta[property="og:title"]').getAttribute('content') || 'pingcode-blog分享';
+      const desc = document.querySelector('meta[property="og:description"]').getAttribute('content') || 'pingcode-desc描述';
+      const imgUrl = location.origin + '/favicon.ico';
+      wx.ready(function () {
+        wx.updateAppMessageShareData({
+          title, // 分享标题
+          desc, // 分享描述
+          imgUrl, // 分享图标
+          success: function () {
+            // 设置成功
+            console.log(' updateAppMessageShareData 成功');
+          }
+        })
+        wx.updateTimelineShareData({
+          title, // 分享标题
+          imgUrl, // 分享图标
+          success: function () {
+            // 设置成功
+            console.log(' updateTimelineShareData 成功');
+          }
+        })
+      });
+      wx.error(function (res) {
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+        console.error(res);
+      });
+    });
+}
+
 module.exports = {
   init: function () {
+    getShareConfig();
     checkWxProgram();
     headerMenu();
     // logoHoverSwitchPanel();
